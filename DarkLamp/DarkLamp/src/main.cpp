cee73c810cc147170a518c18070bbe137ef66dbf
Radim Kučera <radim.kucera@o2.cz>
2023-11-24 17:46:33 +0100

    sanitize

--- DarkLamp/DarkLamp/src/main.cpp
+++ DarkLamp/DarkLamp/src/main.cpp
@@ -4,25 +4,40 @@
 #include <TM16xxDisplay.h>
 #include <U8g2lib.h>
 
-#define RELE1 3
-#define RELE2 4
+// -----------------------------------------
+
+/* PINOUT */
+#define RELE1  3
+#define RELE2  4
+#define LK_STB 7
+#define LK_DIO 8
+#define LK_CLK 9
+
+/* Variables */
+#define LK_DIGITS 8
+
+// -----------------------------------------
 
 /* Key&LED Module */
 //TM1638Anode module1(25, 26, 27, true, 7); // DIO=8, CLK=9, STB=7, activateDisplay, intensity
-TM1638Anode module1(25, 26, 27);
-TM16xxDisplay display1(&module1, 6);    // TM16xx object, 8 digits
+TM1638Anode ledkey(25, 26, 27, true, 7);
+TM16xxDisplay display_segment(&ledkey, LK_DIGITS);    // TM16xx object, 8 digits
 
 /* OLED Display */
-U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SH1106_128X64_NONAME_1_HW_I2C display_oled(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+// -----------------------------------------
 
 void setup(void) {
+
   pinMode(RELE1, OUTPUT);
   pinMode(RELE2, OUTPUT);
   /* u8g2.begin() is required and will sent the setup/init sequence to the display */
-  u8g2.begin();
+  display_oled.begin();
 
   // test display
-  display1.println("12.34.5.6");
+  display_segment.println("12.34.5.6");
+
 }
 
 int nCount=0;
@@ -32,7 +47,7 @@ void loop(void) {
 
   /* L&K Display */ 
   // delay(1000);
-  //  display1.println((String)"Count:" + nCount);
+  //  ledkey_display.println((String)"Count:" + nCount);
   //  nCount++;
 
   /* Relays */
@@ -46,15 +61,15 @@ void loop(void) {
   // delay(1000);                    //1 s čekání
 
   /* OLED display */
-  u8g2.firstPage();
+  display_oled.firstPage();
   do {
-    u8g2.setFont(u8g2_font_ncenB14_tr);
-    u8g2.drawStr(0,20,"Hello World!");
+    display_oled.setFont(u8g2_font_ncenB14_tr);
+    display_oled.drawStr(0,20,"Hello World!");
     for (int i = 0; i <= 60; i++) {
-      u8g2.drawCircle(60, 30, i);
+      display_oled.drawCircle(60, 30, i);
       delay(100);
     }
 
-  } while ( u8g2.nextPage() );
+  } while ( display_oled.nextPage() );
   delay(1000);
 }
