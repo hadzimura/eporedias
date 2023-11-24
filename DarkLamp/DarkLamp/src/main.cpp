626d7888e33897eebd199da646e225e68e702181
Radim Kučera <radim.kucera@o2.cz>
2023-11-24 17:37:17 +0100

    Added modules

--- DarkLamp/DarkLamp/src/main.cpp
+++ DarkLamp/DarkLamp/src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <TM1638Anode.h>
+#include <TM16xxDisplay.h>
 #include <U8g2lib.h>
 
 #define RELE1 3
 #define RELE2 4
 
-/* Constructor for OLED Display */
+/* Key&LED Module */
+//TM1638Anode module1(25, 26, 27, true, 7); // DIO=8, CLK=9, STB=7, activateDisplay, intensity
+TM1638Anode module1(25, 26, 27);
+TM16xxDisplay display1(&module1, 6);    // TM16xx object, 8 digits
+
+/* OLED Display */
 U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup(void) {
@@ -13,12 +20,22 @@ void setup(void) {
   pinMode(RELE2, OUTPUT);
   /* u8g2.begin() is required and will sent the setup/init sequence to the display */
   u8g2.begin();
+
+  // test display
+  display1.println("12.34.5.6");
 }
+
+int nCount=0;
 
 /* draw something on the display with the `firstPage()`/`nextPage()` loop*/
 void loop(void) {
 
+  /* L&K Display */ 
+  // delay(1000);
+  //  display1.println((String)"Count:" + nCount);
+  //  nCount++;
 
+  /* Relays */
   // digitalWrite(RELE1, HIGH);  //relé1 rozepnuto (záleží také na typu)
   // delay(1000);                    //1 s čekání
   // digitalWrite(RELE2_PIN, HIGH);  //relé2 rozepnuto (záleží také na typu)
@@ -28,7 +45,7 @@ void loop(void) {
   // digitalWrite(RELE2_PIN, LOW);   //relé2 sepnuto (záleží také na typu relé)
   // delay(1000);                    //1 s čekání
 
-
+  /* OLED display */
   u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_ncenB14_tr);
